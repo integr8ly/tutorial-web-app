925630a2f72da3bb65a457495b2d7e1bb6a8928c
David Martin <david.martin@redhat.com>
2018-08-30 22:07:09 +0100

    Add a simple server with a dynamic config.js endpoint.
    The local development flow remains unchanged with this commit.
    The below can still be used.
    
    ```
    yarn install;yarn start:dev
    ```
    
    What has changed is what gets started with above command.
    A simple node.js express server (served from `server.js`)
    starts on port `5001`.
    The react dev server proxies any requests it can't deliver statically
    to this express server.
    
    Currently, there is only 1 endpoint in this express app `/config.js`.
    This endpoint returns some configuration to be used when making API
    calls to an OpenShift cluster:
    
    * openshiftAPIUrl (configured from OPENSHIFT_MASTER_URL env var)
    * accessToken (as sent on from the oauth proxy after successful OAuth flow)
    
    In a production scenario, the express server will replace where nginx is
    currently used (future PR to update openshift templates).
    The express server will serve the compiled/built react files when the
    `NODE_ENV` env var is set to `production`.
    These will be served from the `build` folder.

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = process.env.PORT || 5001
+
+// Dynamic configuration for openshift API calls
+app.get('/config.js', (req, res) => {
+  res.send(`window.OPENSHIFT_CONFIG = {
+    clientId: '${process.env.OPENSHIFT_OAUTHCLIENT_ID}',
+    accessTokenUri: 'https://${process.env.OPENSHIFT_HOST}/oauth/token',
+    authorizationUri: 'https://${process.env.OPENSHIFT_HOST}/oauth/authorize',
+    redirectUri: '${process.env.REDIRECT_HOST}',
+    scopes: ['user:full'],
+    masterUri: 'https://${process.env.OPENSHIFT_HOST}'
+  };`)
+})
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'build')))
+  // Handle React routing, return all requests to React app
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+  })
+}
+
+app.listen(port, () => console.log(`Listening on port ${port}`))
