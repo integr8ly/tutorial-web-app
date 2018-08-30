4f84a136b7a5cbcc62483e3a3f29e777a18e527e
Patrick Riley <priley86@gmail.com>
2018-08-30 10:11:20 -0400

    update title/typo

--- src/pages/landing/landingPageMastHead.js
+++ src/pages/landing/landingPageMastHead.js
@@ -1,11 +1,16 @@
 import * as React from 'react';
-import { Grid, Row, Col } from 'patternfly-react';
 
 const LandingPageMastHead = () => (
   <header className="app-landing-page-integr8ly-masthead">
     <div className="container">
-      <h2>Welcome to the <b>Red Hat evaluation enviornment</b></h2>
-      <p>Get started with an end-to-end solution walkthrough<br/>or use any of the available application services to create custom integrations.</p>
+      <h2>
+        Welcome to the <b>Red Hat evaluation environment</b>
+      </h2>
+      <p>
+        Get started with an end-to-end solution walkthrough
+        <br />
+        or use any of the available application services to create custom integrations.
+      </p>
     </div>
   </header>
 );
