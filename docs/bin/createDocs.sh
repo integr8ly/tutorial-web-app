91ad8dfacbc1fa6cc33cf2e202150f3dccc9c100
Paul Wright <pwright@redhat.com>
2018-09-06 18:05:48 +0100

    scripts in progress

---
+++ docs/bin/createDocs.sh
@@ -0,0 +1,9 @@
+sourceDir="../modules/ROOT/pages/_partials/"
+destDir="../../public/steelthreads/asciidocs/en/"
+
+while read p; do
+  echo "$sourceDir$p"
+  ./asciidoc-coalescer.rb $sourceDir$p > $destDir$p
+done <listAll.txt
+
+
