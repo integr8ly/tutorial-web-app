18e789dd84165a23fa537e9cf0d2262ad9b762b9
Adam Jolicoeur <adam.jolicoeur@redhat.com>
2018-09-05 21:02:33 -0400

    add new banner image and content

--- src/pages/landing/landingPageMastHead.js
+++ src/pages/landing/landingPageMastHead.js
@@ -2,14 +2,14 @@ import * as React from 'react';
 
 const LandingPageMastHead = () => (
   <header className="row app-landing-page-integr8ly-masthead">
-    <div className="container-fluid">
+    <div className="col-xs-12">
       <h2>
-        Welcome to the <b>Red Hat evaluation environment</b>
+        Welcome to the <b>Red Hat solution explorer</b>
       </h2>
       <p>
-        Get started with an end-to-end solution walkthrough
+        Get started with an end-to-end solution walkthrough or
         <br />
-        or use any of the available application services to create custom integrations.
+        use any of the available application services to create custom integrations.
       </p>
     </div>
   </header>
