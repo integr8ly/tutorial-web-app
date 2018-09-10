3e341597d1de37653e36d83fef6aab0ace060052
Patrick Riley <priley86@gmail.com>
2018-09-09 21:31:56 -0400

    simulate production

---
+++ public/config.js
@@ -0,0 +1,27 @@
+window.OPENSHIFT_CONFIG = {
+  mockData: {
+    listProvisionedServices: [
+      {
+        appName: 'Red Hat Fuse Online',
+        appDescription:
+          'An integration Platform-as-a-Service (iPaaS) solution that makes it easy for business users to collaborate with integration experts and application developers.  Both low-code environment and developer-focused features are available in this environment.',
+        appLink: 'https://cluster-lfa3xlh.opentry.me'
+      },
+      {
+        appName: 'Red Hat Launcher',
+        appDescription: 'Continuous application delivery, built and deployed on OpenShift.',
+        appLink: 'http://launcher-launcher.cluster-lfa3xlh.opentry.me/'
+      },
+      {
+        appName: 'Eclipse Che',
+        appDescription: 'A developer workspace server and cloud IDE.',
+        appLink: 'https://che-che.cluster-lfa3xlh.opentry.me/dashboard/'
+      },
+      {
+        appName: 'EnMasse',
+        appDescription: 'Managed, self-service messaging on Kubernetes.',
+        appLink: 'https://console-enmasse.cluster-lfa3xlh.opentry.me/console/eval'
+      }
+    ]
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import 'patternfly/dist/css/patternfly.css';
 import 'patternfly/dist/css/patternfly-additions.css';
 import './styles/.css/index.css';
