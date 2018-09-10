90b147d1398b898b639192dfe312a36ab4169d21
Adam Jolicoeur <mindreeper2420@gmail.com>
2018-09-10 10:35:11 -0400

    update Application Services list

--- public/config.js
+++ public/config.js
@@ -2,25 +2,38 @@ window.OPENSHIFT_CONFIG = {
   mockData: {
     listProvisionedServices: [
       {
-        appName: 'Red Hat Fuse Online',
+        appName: 'Red Hat OpenShift',
         appDescription:
-          'An integration Platform-as-a-Service (iPaaS) solution that makes it easy for business users to collaborate with integration experts and application developers.  Both low-code environment and developer-focused features are available in this environment.',
+          'An enterprise-ready container platform. Everything you build will be hosted on the same OpenShift cluster.',
+        appLink: 'https://openshift.com'
+      },
+      {
+        appName: 'Red Hat 3scale API Management Platform',
+        appDescription:
+          'A portal that allows you to define desired authenthication methods, set rate limits, get analytics on the user of your APIs, and create a developer portal for API consumers.',
         appLink: 'https://cluster-lfa3xlh.opentry.me'
       },
       {
-        appName: 'Red Hat Launcher',
-        appDescription: 'Continuous application delivery, built and deployed on OpenShift.',
+        appName: 'Red Hat AMQ',
+        appDescription:
+          'Managed self-service messaing on Kubernetes, AMQ Online (Tech Preview) and Red Hat AMQ Broker are available in this environment.',
+        appLink: 'https://cluster-lfa3xlh.opentry.me'
+      },
+      {
+        appName: 'Red Hat Fuse',
+        appDescription:
+          'An integration platform-as-a-serviceBoth low-code environment and developer-focused features are available in this environment.',
+        appLink: 'https://cluster-lfa3xlh.opentry.me'
+      },
+      {
+        appName: 'Red Hat OpenShift Application Runtimes',
+        appDescription: 'A collection of cloud-native runtimes for developing Java &trade; or JavaScript applications on OpenShift.',
         appLink: 'http://launcher-launcher.cluster-lfa3xlh.opentry.me/'
       },
       {
         appName: 'Eclipse Che',
         appDescription: 'A developer workspace server and cloud IDE.',
         appLink: 'https://che-che.cluster-lfa3xlh.opentry.me/dashboard/'
-      },
-      {
-        appName: 'EnMasse',
-        appDescription: 'Managed, self-service messaging on Kubernetes.',
-        appLink: 'https://console-enmasse.cluster-lfa3xlh.opentry.me/console/eval'
       }
     ]
   }
