450667bb561be46b8e1c626983cd5a1f54acb8fa
Evan Shortiss <evanshortiss@gmail.com>
2018-09-11 16:10:35 -0700

    feat: programmatically detect openshift host if possible

--- server.js
+++ server.js
@@ -1,12 +1,36 @@
-const express = require('express');
-const path = require('path');
+const express = require('express')
+const path = require('path')
+const axios = require('axios')
+const app = express()
+const port = process.env.PORT || 5001
 
-const app = express();
-const port = process.env.PORT || 5001;
+async function getOpenShiftHostUrl() {
+  const response = await axios.get('https://kubernetes.default/.well-known/oauth-authorization-server')
+
+  if (response.status !== 200) {
+    throw new Error(`received unexpected ${response.status} status when requesting openshift host URL`)
+  } else if (!response.data || !response.data.issuer) {
+    throw new Error('received unexpected response when requesting openshift host URL')
+  } else {
+    return response.data.issuer
+  }
+}
 
 // Dynamic configuration for openshift API calls
-app.get('/config.js', (req, res) => {
-  if (!process.env.OPENSHIFT_HOST) {
+app.get('/config.js', async (req, res) => {
+  let { OPENSHIFT_HOST } = process.env
+
+  if (!OPENSHIFT_HOST) {
+    console.log('OPENSHIFT_HOST environment variable not set. attempting to fetch host programmatically')
+
+    try {
+      OPENSHIFT_HOST = await getOpenShiftHostUrl()
+    } catch (e) {
+      console.warn('unable to programmatically determine OPENSHIFT_HOST')
+    }
+  }
+
+  if (!OPENSHIFT_HOST) {
     console.warn('OPENSHIFT_HOST not set. Using service URLs from env vars');
     res.send(`window.OPENSHIFT_CONFIG = {
       mockData: {
@@ -77,8 +101,8 @@ app.get('/config.js', (req, res) => {
     }
     res.send(`window.OPENSHIFT_CONFIG = {
       clientId: '${process.env.OPENSHIFT_OAUTHCLIENT_ID}',
-      accessTokenUri: 'https://${process.env.OPENSHIFT_HOST}/oauth/token',
-      authorizationUri: 'https://${process.env.OPENSHIFT_HOST}/oauth/authorize',
+      accessTokenUri: 'https://${OPENSHIFT_HOST}/oauth/token',
+      authorizationUri: 'https://${OPENSHIFT_HOST}/oauth/authorize',
       redirectUri: '${redirectHost}/oauth/callback',
       scopes: ['user:full'],
       masterUri: 'https://${process.env.OPENSHIFT_HOST}',
