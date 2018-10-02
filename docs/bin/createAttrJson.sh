0510d16720eec8a719421d2281881ae07dd34856
Paul Wright <pwright@redhat.com>
2018-10-02 17:58:32 +0100

    wip

--- docs/bin/createAttrJson.sh
+++ docs/bin/createAttrJson.sh
@@ -1,19 +1,25 @@
-sourceDir="../modules/ROOT/pages/_partials/"
+# usage:
+# createAttrjson.sh <walkthrough-id>
+
+sourceFile="../modules/ROOT/pages/_partials/attributes-$1.adoc"
 destDir="../../public/steelthreads/json/en/"
 
 
 
-./asciidoc-coalescer.rb $sourceDir$1 > $destDir$1.
+./asciidoc-coalescer.rb $sourceFile > $destDir$1.tmp
 
+echo {
 
 while IFS=':' read dummy var val
 do
 if [ "$var" ]
 then
 value="$(echo -e "${var}" | sed -e 's/^[[:space:]]*//')"
-        echo \"$var\",\"$value\",
+        echo \"$var\":\"$value\",
 fi
 
-done < $destDir$1
+done < $destDir$1.tmp
 
-cat $destDir$1|jq -n -r ''
+echo \"dummy\":\"value\"
+
+echo }
