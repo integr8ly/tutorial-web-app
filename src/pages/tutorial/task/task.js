4a850c7fa77f9cb0fb5ba0a02aef19b209c400e4
Aiden Keating <akeating@redhat.com>
2018-10-03 14:44:16 +0100

    Use top level task attributes if available

--- src/pages/tutorial/task/task.js
+++ src/pages/tutorial/task/task.js
@@ -196,7 +196,7 @@ class TaskPage extends React.Component {
                       <React.Fragment key={i}>
                         <AsciiDocTemplate
                           adoc={step.stepDoc}
-                          attributes={Object.assign({}, step.attributes, this.getDocsAttributes())}
+                          attributes={Object.assign({}, threadTask.attributes, step.attributes, this.getDocsAttributes())}
                         />
                         {step.infoVerifications &&
                           step.infoVerifications.map((verification, j) => (
@@ -210,7 +210,7 @@ class TaskPage extends React.Component {
                               >
                                 <AsciiDocTemplate
                                   adoc={verification}
-                                  attributes={Object.assign({}, step.attributes, this.getDocsAttributes())}
+                                  attributes={Object.assign({}, threadTask.attributes, step.attributes, this.getDocsAttributes())}
                                 />
                               </Checkbox>
                             </Alert>
@@ -221,7 +221,7 @@ class TaskPage extends React.Component {
                               <strong>{t('task.verificationTitle')}</strong>
                               <AsciiDocTemplate
                                 adoc={verification}
-                                attributes={Object.assign({}, step.attributes, this.getDocsAttributes())}
+                                attributes={Object.assign({}, threadTask.attributes, step.attributes, this.getDocsAttributes())}
                               />
                             </Alert>
                           ))}
