a168fac974b4eada38586e371125a35231902d99
Adam Jolicoeur <mindreeper2420@gmail.com>
2018-10-30 10:52:08 -0400

    Remove reference to CSS files

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
-// import 'patternfly/dist/css/rcue.css';
-// import 'patternfly/dist/css/rcue-additions.css';
-// import '@fortawesome/fontawesome-free/css/fontawesome.min.css';
-import '@patternfly/patternfly-next/patternfly.min.css';
 import './styles/.css/index.css';
 
 import App from './App';
