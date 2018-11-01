4bbbb7eb61a89b06aa684a547d0abc0f9d60e00f
Adam Jolicoeur <mindreeper2420@gmail.com>
2018-11-01 16:13:07 -0400

    feat(PF4): Introduce PatternFly 4

--- src/components/breadcrumb/breadcrumb.js
+++ src/components/breadcrumb/breadcrumb.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { translate } from 'react-i18next';
-import { Icon, Breadcrumb as PfBreadcrumb } from 'patternfly-react';
+import { Icon } from 'patternfly-react';
+import { BreadcrumbItem, Breadcrumb as PfBreadcrumb } from '@patternfly/react-core';
 import { withRouter } from 'react-router-dom';
 
 class Breadcrumb extends React.Component {
@@ -16,15 +17,15 @@ class Breadcrumb extends React.Component {
     const { t, threadName, threadId, totalTasks, taskPosition } = this.props;
     return (
       <PfBreadcrumb className="integr8ly-breadcrumb">
-        <PfBreadcrumb.Item onClick={this.homeClicked}>
+        <BreadcrumbItem onClick={this.homeClicked} className="integr8ly-breadcrumb-home">
           <Icon className="fa-lg" type="fa" name="home" />
-        </PfBreadcrumb.Item>
-        {threadName && !taskPosition && <PfBreadcrumb.Item active>{threadName}</PfBreadcrumb.Item>}
+        </BreadcrumbItem>
+        {threadName && !taskPosition && <BreadcrumbItem isActive>{threadName}</BreadcrumbItem>}
         {threadName &&
           taskPosition && (
             <React.Fragment>
-              <PfBreadcrumb.Item href={`/tutorial/${threadId}`}>{threadName}</PfBreadcrumb.Item>
-              <PfBreadcrumb.Item active>{t('breadcrumb.task', { taskPosition, totalTasks })}</PfBreadcrumb.Item>
+              <BreadcrumbItem to={`/tutorial/${threadId}`}>{threadName}</BreadcrumbItem>
+              <BreadcrumbItem isActive>{t('breadcrumb.task', { taskPosition, totalTasks })}</BreadcrumbItem>
             </React.Fragment>
           )}
       </PfBreadcrumb>
@@ -33,12 +34,19 @@ class Breadcrumb extends React.Component {
 }
 
 Breadcrumb.propTypes = {
+  /**  Navigation history */
   history: PropTypes.object,
+  /** Translation function provided for i18n */
   t: PropTypes.func.isRequired,
+  /** Walkthrough name (thread) that will show up in the breadcrumb link */
   threadName: PropTypes.string,
+  /** Walkthrough ID (thread) */
   threadId: PropTypes.string,
+  /** Current task in the walkthrough that the user is on */
   taskPosition: PropTypes.number,
+  /** The total number of tasks for this walkthrough */
   totalTasks: PropTypes.number,
+  /** Called when the home button is clicked */
   homeClickedCallback: PropTypes.func
 };
 
