234a07ef539672dbf60088192414af37c64619dd
Adam Jolicoeur <mindreeper2420@gmail.com>
2018-11-06 09:07:21 -0500

    Convert Labels to PF4 styling

--- src/components/installedAppsView/InstalledAppsView.js
+++ src/components/installedAppsView/InstalledAppsView.js
@@ -87,7 +87,7 @@ class InstalledAppsView extends React.Component {
       <li onClick={() => window.open(`${customApp.url}`, '_blank')} key={`openshift_console_${i}`} value={i}>
         <div className="integr8ly-installed-apps-view-title">
           <p>{customApp.name}</p>
-          <span className="integr8ly-label-community">custom</span>
+          <span className="pf-c-label integr8ly-label-community">custom</span>
         </div>
         <div className="integr8ly-state-ready">
           <Icon type="fa" name="bolt" /> &nbsp;Ready for use
@@ -107,7 +107,7 @@ class InstalledAppsView extends React.Component {
         >
           <div className="integr8ly-installed-apps-view-title">
             <p>{prettyName}</p>
-            <span className={`integr8ly-label-${gaStatus}`}>{gaStatus}</span>
+            <span className={`pf-c-label integr8ly-label-${gaStatus}`}>{gaStatus}</span>
           </div>
           {InstalledAppsView.getStatusForApp(app)}
           <small />
