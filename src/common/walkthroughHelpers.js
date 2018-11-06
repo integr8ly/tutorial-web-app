d8996321bb417a501bc7c3c53a59773503e7e5c8
Aiden Keating <akeating@redhat.com>
2018-11-06 09:43:30 +0000

    Result of lint auto fix

--- src/common/walkthroughHelpers.js
+++ src/common/walkthroughHelpers.js
@@ -4,7 +4,7 @@ const CONTEXT_PREAMBLE = 'preamble';
 const CONTEXT_SECTION = 'section';
 const CONTEXT_PARAGRAPH = 'paragraph';
 
-const retrieveOverviewFromAdoc = (rawAdoc) => {
+const retrieveOverviewFromAdoc = rawAdoc => {
   const parsedAdoc = parseAdoc(rawAdoc);
   return {
     title: parsedAdoc.getDocumentTitle(),
@@ -16,10 +16,10 @@ const retrieveOverviewFromAdoc = (rawAdoc) => {
       shortDescription: shortDescriptionFromBlock(b),
       steps: getStepsForTask(b)
     }))
-  }
-}
+  };
+};
 
-const getTotalWalkthroughTime = (adoc) => {
+const getTotalWalkthroughTime = adoc => {
   let time = 0;
   adoc.blocks.forEach(b => {
     if (b.context === CONTEXT_PREAMBLE || b.context === CONTEXT_PARAGRAPH) {
@@ -28,44 +28,40 @@ const getTotalWalkthroughTime = (adoc) => {
     time += parseInt(b.getAttribute('time')) || 0;
   });
   return time;
-}
+};
 
-const getStepsForTask = (task) => {
-  return task.blocks.map(b => {
-    if (b.context === CONTEXT_PARAGRAPH || b.context === CONTEXT_PREAMBLE) {
-      return;
-    }
-    return {
-      title: b.title,
-      isVerification: b.getAttribute('verification') === 'true',
-      bodyHTML: b.convert(),
-      blocks: getBlocksForStep(b)
-    }
-  }).filter(b => !!b);
-}
+const getStepsForTask = task =>
+  task.blocks
+    .map(b => {
+      if (b.context === CONTEXT_PARAGRAPH || b.context === CONTEXT_PREAMBLE) {
+        return;
+      }
+      return {
+        title: b.title,
+        isVerification: b.getAttribute('verification') === 'true',
+        bodyHTML: b.convert(),
+        blocks: getBlocksForStep(b)
+      };
+    })
+    .filter(b => !!b);
 
-const getBlocksForStep = (step) => {
-  return step.blocks.map(b => {
-    return {
-      isVerification: !!b.getAttribute('verification'),
-      verificationId: b.getAttribute('verification'),
-      verificationFailText: b.getAttribute('verificationFailText'),
-      bodyHTML: b.convert()
-    }
-  })
-}
+const getBlocksForStep = step =>
+  step.blocks.map(b => ({
+    isVerification: !!b.getAttribute('verification'),
+    verificationId: b.getAttribute('verification'),
+    verificationFailText: b.getAttribute('verificationFailText'),
+    bodyHTML: b.convert()
+  }));
 
-const retrieveTasksFromAdoc = (adoc) => {
-  return adoc.blocks.filter(b => b.context === CONTEXT_SECTION);
-}
+const retrieveTasksFromAdoc = adoc => adoc.blocks.filter(b => b.context === CONTEXT_SECTION);
 
-const shortDescriptionFromBlock = (block) => {
+const shortDescriptionFromBlock = block => {
   if (block.blocks[0].context !== CONTEXT_PARAGRAPH || block.blocks[0].lines.length === 0) {
     return;
   }
   return block.blocks[0].lines[0];
-}
+};
 
-const parseAdoc = (rawAdoc) => asciidoctor().load(rawAdoc);
+const parseAdoc = rawAdoc => asciidoctor().load(rawAdoc);
 
 export { retrieveOverviewFromAdoc };
