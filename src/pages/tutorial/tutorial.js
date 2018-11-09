62c986a1eb32c98b5d49d77247dfffc608e0d0a9
Peter Braun <pbraun@redhat.com>
2018-11-09 10:45:02 +0100

    add walkthrough manifest and init action

--- src/pages/tutorial/tutorial.js
+++ src/pages/tutorial/tutorial.js
@@ -4,7 +4,6 @@ import { withRouter } from 'react-router-dom';
 import { translate } from 'react-i18next';
 import { noop, Button, Grid, Icon, ListView } from 'patternfly-react';
 import { connect, reduxActions } from '../../redux';
-import AsciiDocTemplate from '../../components/asciiDocTemplate/asciiDocTemplate';
 import PfMasthead from '../../components/masthead/masthead';
 import WalkthroughResources from '../../components/walkthroughResources/walkthroughResources';
 import { parseWalkthroughAdoc } from '../../common/walkthroughHelpers';
@@ -166,7 +165,8 @@ TutorialPage.propTypes = {
     params: PropTypes.object
   }),
   getThread: PropTypes.func,
-  thread: PropTypes.object
+  thread: PropTypes.object,
+  getWalkthrough: PropTypes.func
 };
 
 TutorialPage.defaultProps = {
@@ -180,7 +180,8 @@ TutorialPage.defaultProps = {
     params: {}
   },
   getThread: noop,
-  thread: null
+  thread: null,
+  getWalkthrough: noop
 };
 
 const mapDispatchToProps = dispatch => ({
