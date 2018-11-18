0b145c12f3a95fc8ba08bc6801de646eaa2d5f9d
Aiden Keating <akeating@redhat.com>
2018-11-18 18:14:25 +0000

    Resolve failing builds

--- src/components/walkthroughResources/walkthroughResources.js
+++ src/components/walkthroughResources/walkthroughResources.js
@@ -1,21 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Icon } from 'patternfly-react';
 import { connect } from '../../redux';
 import { getDashboardUrl } from '../../common/serviceInstanceHelpers';
 
 class WalkthroughResources extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      resourceList: null
-    };
-  }
-
-  componentDidMount() {
-    // this.buildResourceList();
-  }
-
   mapServiceLinks() {
     const { resources, middlewareServices } = this.props;
     if (resources.length !== 0) {
@@ -53,48 +41,6 @@ class WalkthroughResources extends React.Component {
       });
     }
     return null;
-  }
-
-  buildResourceList() {
-    const resources = this.mapServiceLinks();
-    let resourceList = null;
-    if (resources && resources.length !== 0) {
-      resourceList = resources.map(resource => {
-        const resourceLinks = resource.links.map(link => (
-          <li key={link.name}>
-            <a href={link.url} target="top">
-              {link.name}
-            </a>
-          </li>
-        ));
-        return (
-          <div key={resource.title}>
-            <h4 className="integr8ly-helpful-links-product-title">
-              <Icon
-                className={resource.serviceStatus ? 'integr8ly-state-ready' : 'integr8ly-state-unavailable'}
-                type="pf"
-                name={resource.serviceStatus ? 'on-running' : 'error-circle-o'}
-              />
-              &nbsp;
-              {resource.title}
-              &nbsp;
-              {resource.gaStatus === 'community' ? (
-                <span className="integr8ly-label-community integr8ly-walkthrough-labels-tag">community</span>
-              ) : (
-                <span />
-              )}
-              {resource.gaStatus === 'preview' ? (
-                <span className="integr8ly-label-preview integr8ly-walkthrough-labels-tag">preview</span>
-              ) : (
-                <span />
-              )}
-            </h4>
-            <ul className="list-unstyled">{resourceLinks}</ul>
-          </div>
-        );
-      });
-    }
-    this.setState({ resourceList });
   }
 
   render() {
