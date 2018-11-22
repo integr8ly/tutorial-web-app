a4bf00172e26b33c32a89696745724a635386b29
Aiden Keating <akeating@redhat.com>
2018-11-22 11:52:19 +0000

    Expose custom routes to the end-user walkthroughs

--- src/services/walkthroughServices.js
+++ src/services/walkthroughServices.js
@@ -1,18 +1,9 @@
 import axios from 'axios';
 import serviceConfig from './config';
-import { watch, currentUser } from './openshiftServices';
+import { watch, currentUser, OpenShiftWatchEvents } from './openshiftServices';
 import { initCustomThread } from './threadServices';
 import { buildValidProjectNamespaceName, findOrCreateOpenshiftResource } from '../common/openshiftHelpers';
-import {
-  buildServiceInstanceCompareFn,
-  buildServiceInstanceResourceObj,
-  CRUDAppInstanceTransform,
-  DEFAULT_SERVICES,
-  handleWalkthroughOneRoutes,
-  handleWalkthroughTwoRoutes,
-  handleServiceInstancesProvision,
-  MessagingAppServiceInstanceTransform
-} from '../common/serviceInstanceHelpers';
+import { buildServiceInstanceCompareFn } from '../common/serviceInstanceHelpers';
 import {
   namespaceResource,
   namespaceRequestResource,
@@ -21,63 +12,7 @@ import {
   serviceInstanceDef,
   routeDef
 } from '../common/openshiftResourceDefinitions';
-import { REJECTED_ACTION } from '../redux/helpers';
-import { GET_THREAD } from '../redux/constants/threadConstants';
-
-const WALKTHROUGH_IDS = { ONE: '1', ONE_A: '1A', TWO: '2' };
-
-/**
- * Walkthroughs definitions, each root level object represents one walkthrough, each contains:
- * - suffix of the namespace ${username}-${suffix}
- * - list of services needed for the walkthrough, listed by service catalog names
- * - ServiceInstances transforms allowing to customise the services e.g. add parameters
- */
-const walkthroughs = {
-  one: {
-    id: WALKTHROUGH_IDS.ONE,
-    namespaceSuffix: 'walkthrough-one',
-    services: [DEFAULT_SERVICES.CRUD_APP, DEFAULT_SERVICES.MESSAGING_APP],
-    transforms: [new CRUDAppInstanceTransform(), new MessagingAppServiceInstanceTransform()],
-    watchers: [
-      {
-        resource: namespace => routeDef(namespace),
-        handlerFn: handleWalkthroughOneRoutes.bind(null, 'walkthrough-one')
-      },
-      {
-        resource: namespace => serviceInstanceDef(namespace),
-        handlerFn: handleServiceInstancesProvision.bind(null, 'walkthrough-one')
-      }
-    ]
-  },
-  oneA: {
-    id: WALKTHROUGH_IDS.ONE_A,
-    namespaceSuffix: 'walkthrough-one-a',
-    services: [DEFAULT_SERVICES.CRUD_APP, DEFAULT_SERVICES.MESSAGING_APP],
-    transforms: [new CRUDAppInstanceTransform(), new MessagingAppServiceInstanceTransform()],
-    watchers: [
-      {
-        resource: namespace => routeDef(namespace),
-        handlerFn: handleWalkthroughOneRoutes.bind(null, 'walkthrough-one-a')
-      },
-      {
-        resource: namespace => serviceInstanceDef(namespace),
-        handlerFn: handleServiceInstancesProvision.bind(null, 'walkthrough-one-a')
-      }
-    ]
-  },
-  two: {
-    id: WALKTHROUGH_IDS.TWO,
-    namespaceSuffix: 'walkthrough-two',
-    services: [],
-    transforms: [],
-    watchers: [
-      {
-        resource: namespace => routeDef(namespace),
-        handlerFn: handleWalkthroughTwoRoutes.bind(null, 'walkthrough-two')
-      }
-    ]
-  }
-};
+import { addWalkthroughService, removeWalkthroughService } from '../redux/actions/walkthroughServiceActions';
 
 const DEFAULT_SERVICE_INSTANCE = {
   kind: 'ServiceInstance',
@@ -87,6 +22,14 @@ const DEFAULT_SERVICE_INSTANCE = {
   }
 };
 
+/**
+ * Provision a namespace if it does not already exist. Once the namespace is
+ * created the ServiceInstances associated with the walkthrough will be
+ * provisioned in the namespace.
+ *
+ * @param {Function} dispatch Redux dispatch.
+ * @param {string} walkthoughName The identifier of the walkthrough to provision.
+ */
 const prepareCustomWalkthroughNamespace = (dispatch, walkthoughName) => {
   if (window.OPENSHIFT_CONFIG.mockData) {
     return Promise.resolve([]);
@@ -117,76 +60,37 @@ const prepareCustomWalkthroughNamespace = (dispatch, walkthoughName) => {
                 buildServiceInstanceCompareFn(siObj)
               )
             )
-          );
+          )
+            .then(() => watch(routeDef(userNamespace)))
+            .then(watchListener =>
+              watchListener.onEvent(handleResourceWatchEvent.bind(null, dispatch, walkthoughName))
+            );
         });
       });
     });
 };
 
 /**
- * Prepare namespace for the given walkthrough:
- * - Setup a user-specific projects namespace if it doesn't already exist. This
- *   will house the users services.
- * - Provision one of each walkthrough services into the namespace
- * @param dispatch
- * @param walkthrough
- * @param siInfoOtherData other data that will be added as siInfo.otherData ={} object, can be used to pass data into transforms
+ * Default handle for a watch event on an OpenShift resource. If a resource is
+ * of a type that can be watched/parsed by the webapp then this will dispatch
+ * appropriate actions for the OpenShift resource so that it can be handled
+ * elsewhere.
+ *
+ * @param {Function} dispatch Redux dispatch.
+ * @param {string} walkthroughId The identifier for the walkthrough the resource was created from.
+ * @param {Object} event Watch event.
  */
-const prepareWalkthroughNamespace = (dispatch, walkthrough, siInfoOtherData) => {
-  if (window.OPENSHIFT_CONFIG.mockData) {
-    return null;
+const handleResourceWatchEvent = (dispatch, walkthroughId, event) => {
+  if (event.type === OpenShiftWatchEvents.OPENED || event.type === OpenShiftWatchEvents.CLOSED) {
+    return;
   }
-  return currentUser()
-    .then(user => {
-      const userNamespace = buildValidProjectNamespaceName(user.username, walkthrough.namespaceSuffix);
-      const namespaceObj = namespaceResource(userNamespace);
-      const namespaceRequestObj = namespaceRequestResource(userNamespace);
-
-      findOrCreateOpenshiftResource(
-        namespaceDef,
-        namespaceObj,
-        resObj => resObj.metadata.name === userNamespace,
-        namespaceRequestDef,
-        namespaceRequestObj
-      )
-        .then(() => {
-          const siObjs = walkthrough.services.map(name =>
-            buildServiceInstanceResourceObj(
-              {
-                namespace: userNamespace,
-                name,
-                user,
-                otherData: siInfoOtherData
-              },
-              walkthrough.transforms
-            )
-          );
-          return Promise.all(
-            siObjs.map(siObj =>
-              findOrCreateOpenshiftResource(
-                serviceInstanceDef(userNamespace),
-                siObj,
-                buildServiceInstanceCompareFn(siObj)
-              )
-            )
-          );
-        })
-        .then(() => {
-          walkthrough.watchers.forEach(watcher => {
-            watch(watcher.resource(userNamespace)).then(watchListener =>
-              watchListener.onEvent(watcher.handlerFn.bind(null, dispatch))
-            );
-          });
-        });
-    })
-    .catch(err => {
-      dispatch({
-        type: REJECTED_ACTION(GET_THREAD),
-        payload: {
-          error: err
-        }
-      });
-    });
+  if (event.type === OpenShiftWatchEvents.ADDED || event.type === OpenShiftWatchEvents.MODIFIED) {
+    dispatch(addWalkthroughService(walkthroughId, event.payload));
+    return;
+  }
+  if (event.type === OpenShiftWatchEvents.DELETED) {
+    dispatch(removeWalkthroughService(walkthroughId, event.payload));
+  }
 };
 
 /**
@@ -202,16 +106,8 @@ const getWalkthrough = (language, id) =>
   );
 
 /**
- * Retrieves the json document that specifies all available walkthroughs (aka threads)
- * @param {} language Specifies the language end point where the json file is stored.  Used to create multiple localized documenation.
+ * Retrieves a list of walkthroughs from the backend.
  */
-const getWalkthroughs = language =>
-  axios(
-    serviceConfig({
-      url: `${process.env.REACT_APP_STEELTHREAD_JSON_PATH}${language}/threads.json`
-    })
-  );
-
 const getCustomWalkthroughs = () =>
   axios(
     serviceConfig({
@@ -219,12 +115,4 @@ const getCustomWalkthroughs = () =>
     })
   );
 
-export {
-  getWalkthrough,
-  getWalkthroughs,
-  prepareWalkthroughNamespace,
-  walkthroughs,
-  WALKTHROUGH_IDS,
-  getCustomWalkthroughs,
-  prepareCustomWalkthroughNamespace
-};
+export { getWalkthrough, getCustomWalkthroughs, prepareCustomWalkthroughNamespace };
