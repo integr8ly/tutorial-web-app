1e08e343083d71208c78fe51efdf987f43a92e85
Adam Jolicoeur <mindreeper2420@gmail.com>
2018-12-03 10:23:46 -0500

    Fix merge errors and tests

--- src/pages/tutorial/tutorial.js
+++ src/pages/tutorial/tutorial.js
@@ -88,8 +88,7 @@ class TutorialPage extends React.Component {
               <Grid.Col xs={12} sm={9} className="integr8ly-task-container pf-u-mt-lg">
                 <div className="integr8ly-task-dashboard-header">
                   <h3 className="pf-c-title pf-m-2xl">{parsedThread.title}</h3>
-                  <button className="pf-c-button pf-m-primary"
-                    onClick={e => this.getStarted(e, id)}>
+                  <button className="pf-c-button pf-m-primary" onClick={e => this.getStarted(e, id)}>
                     {t('tutorial.getStarted')}
                   </button>
                 </div>
