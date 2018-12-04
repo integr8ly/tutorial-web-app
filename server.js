f0ee9cdcdc684926a812c8027ff0ce5840a5ccb9
Peter Braun <pbraun@redhat.com>
2018-12-04 16:50:39 +0100

    default to the github repo when no location is specified

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const DEFAULT_CUSTOM_CONFIG_DATA = {
   services: []
 };
 
-const walkthroughLocations = process.env.WALKTHROUGH_LOCATIONS || './public/walkthroughs';
+const walkthroughLocations = process.env.WALKTHROUGH_LOCATIONS || 'https://github.com/integr8ly/tutorial-web-app-walkthroughs';
 const IGNORED_WALKTHROUGH_SEARCH_PATHS = ['.git', '.idea', '.DS_Store'];
 
 const CONTEXT_PREAMBLE = 'preamble';
