ae4e92724f0fae0ce2093f86880a39676177a646
Aiden Keating <akeating@redhat.com>
2018-12-06 14:53:38 +0000

    Allow branches and tags to be specified in a git clone
    Currently during a git clone, we only allow cloning the default
    branch of a repo. However, it may be useful for releases to be
    able to have branches and tags specified in the URL.
    
    This change follows the syntax of npm repository dependencies, using
    the hash value of the URL to specify the branch or tag. If none are
    specified it will clone the default branch of the repo.

--- git_client.js
+++ git_client.js
@@ -8,6 +8,33 @@ function getRepoName(repoUrl) {
   return path.basename(parsed.path);
 }
 
+/**
+ * Retrieve the base git repo URL from a provided URL.
+ * 
+ * A URL could contain a hash, which we're interpreting as a branch/tag separator,
+ * we shouldn't include this.
+ **/
+function cleanupRepoUrl(repoUrl) {
+  const parsed = url.parse(repoUrl);
+  return `${parsed.protocol}//${parsed.host}${parsed.path}`;
+}
+
+/**
+ * Retrieve the options for the clone, given the URL.
+ * 
+ * If the URL has a hash at the end of it then this is used as the branch name.
+ */
+function getCloneOptionsForRepo(repoUrl) {
+  let cloneOptions = [ '--depth', 1, '--single-branch' ];
+  const parsed = url.parse(repoUrl);
+  if (!!parsed.hash) {
+    // From experimenting, parsed.hash starts with a hash symbol. Just in case.
+    const branchName = parsed.hash[0] === '#' ? parsed.hash.substring(1) : parsed.hash;
+    cloneOptions = cloneOptions.concat(['--branch', branchName]);
+  }
+  return cloneOptions;
+}
+
 exports.cloneRepo = (repoUrl, targetDir) =>
   new Promise((resolve, reject) => {
     // Ensure directory exists
@@ -17,9 +44,11 @@ exports.cloneRepo = (repoUrl, targetDir) =>
 
     const repoName = getRepoName(repoUrl);
     const clonePath = path.join(targetDir, repoName);
-
+    const cleanRepoUrl = cleanupRepoUrl(repoUrl);
+    const cloneOpts = getCloneOptionsForRepo(repoUrl);
+  
     simpleGit
-      .clone(repoUrl, clonePath, { '--depth': 1 })
+      .clone(cleanRepoUrl, clonePath, cloneOpts)
       .then(() => resolve(clonePath))
       .catch(reject);
   });
