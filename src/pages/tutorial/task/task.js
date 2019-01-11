915024a2c9a4d729d6a7302b4078f41d5d55fd82
Adam Jolicoeur <mindreeper2420@gmail.com>
2019-01-11 14:35:26 -0500

    fix(lists): remove indentation from walkthrough resources list

--- src/pages/tutorial/task/task.js
+++ src/pages/tutorial/task/task.js
@@ -380,7 +380,7 @@ class TaskPage extends React.Component {
                 <div className="integr8ly-module-column pf-c-content pf-u-mt-lg pf-u-px-lg">
 =======
             <Grid.Row className="pf-c-content">
-              <Grid.Col xs={12} sm={9} className="integr8ly-module pf-u-mt-xl pf-u-mb-3xl">
+              <Grid.Col xs={12} sm={9} className="integr8ly-module pf-u-mt-3xl pf-u-mb-3xl">
                 <div className="integr8ly-module-column">
 >>>>>>> feat(PF4): Introduce PatternFly 4
                   <h2>{threadTask.title}</h2>
