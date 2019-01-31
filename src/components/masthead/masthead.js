611dbe3788b989755efae7178462a3ed226c8d13
Mark Franceschelli <mfrances@redhat.com>
2019-01-31 07:18:47 -0500

    fix logout

--- src/components/masthead/masthead.js
+++ src/components/masthead/masthead.js
@@ -12,32 +12,31 @@ import {
 } from '@patternfly/react-core';
 import accessibleStyles from '@patternfly/patternfly-next/utilities/Accessibility/accessibility.css';
 import { css } from '@patternfly/react-styles';
-import { noop, Icon, MenuItem } from 'patternfly-react';
+import { noop } from 'patternfly-react';
 import { withRouter } from 'react-router-dom';
-import { connect, reduxActions, store } from '../../redux';
-import { aboutModalTypes } from '../../redux/constants';
+import { connect, reduxActions } from '../../redux';
 import { logout } from '../../services/openshiftServices';
 import brandImg from '../../img/Logo_RH_SolutionExplorer_White.png';
 
 class Masthead extends React.Component {
-  state = {
-    mobileToggle: true
-  };
+  constructor(props) {
+    super(props);
 
-  onAbout = () => {
-    store.dispatch({
-      type: aboutModalTypes.ABOUT_MODAL_SHOW
-    });
-  };
+    this.state = {
+      isUserDropdownOpen: false
+    };
 
-  onHelp = () => {
-    window.location.href = '/help';
-  };
+    this.onTitleClick = this.onTitleClick.bind(this);
+    this.onLogoutUser = this.onLogoutUser.bind(this);
+
+    this.onUserDropdownToggle = this.onUserDropdownToggle.bind(this);
+    this.onUserDropdownSelect = this.onUserDropdownSelect.bind(this);
+  }
 
   onLogoutUser = () => {
     if (window.OPENSHIFT_CONFIG.mockData) {
       window.localStorage.clear();
-      window.location.href = window.OPENSHIFT_CONFIG.ssoLogoutUri;
+      window.location.href = '/';
       return;
     }
     logout().then(() => {
@@ -46,115 +45,65 @@ class Masthead extends React.Component {
   };
 
   onTitleClick = () => {
+    // debugger;
+    console.log('Logo clicked');
     const { history } = this.props;
+    console.log(history);
     history.push(`/`);
+    console.log(history);
+    // this.props.history.push('/');
+    // console.log(this.props.history);
   };
 
-  onDropdownSelect = event => {
+  onUserDropdownToggle(isUserDropdownOpen) {
     this.setState({
-      isDropdownOpen: !this.state.isDropdownOpen
+      isUserDropdownOpen
+    });
+  }
+
+  onUserDropdownSelect = () => {
+    console.log('Dropdown selected');
+    this.setState({
+      isUserDropdownOpen: !this.state.isUserDropdownOpen
     });
   };
 
-  renderMobileNav() {
-    const { mobileToggle } = this.state;
-
-    if (mobileToggle) {
-      return null;
-    }
-
-    return (
-      <div
-        role="menu"
-        className="nav-pf-vertical nav-pf-vertical-with-sub-menus nav-pf-vertical-with-badges hidden show-mobile-nav"
-        aria-live="polite"
-      >
-        <ul className="list-group">
-          <li className="list-group-item">
-            <a role="menuitem" href="#about" onClick={this.onAbout}>
-              <span className="list-group-item-value">About</span>
-            </a>
-          </li>
-          <li className="list-group-item">
-            <a role="menuitem" href="#help" onClick={this.onHelp}>
-              <span className="list-group-item-value">Help</span>
-            </a>
-          </li>
-          <li className="list-group-item">
-            <a role="menuitem" href="#logout" onClick={this.onLogoutUser}>
-              <span className="list-group-item-value">Logout</span>
-            </a>
-          </li>
-        </ul>
-      </div>
-    );
-  }
-
-  renderActions() {
-    return (
-      <PageHeader.Dropdown id="app-help-dropdown" title={<span aria-hidden className="pficon pficon-help" />}>
-        <MenuItem eventKey="1" onClick={this.onHelp}>
-          Help
-        </MenuItem>
-        <MenuItem eventKey="2" onClick={this.onAbout}>
-          About
-        </MenuItem>
-      </PageHeader.Dropdown>
-    );
-  }
-
-  renderUserDropdown() {
-    const title = (
-      <React.Fragment>
-        <Icon type="pf" name="user" key="user-icon" />{' '}
-        <span className="dropdown-title" key="dropdown-title">
-          {window.localStorage.getItem('currentUserName')} {` `}
-        </span>
-      </React.Fragment>
-    );
-
-    return (
-      <PageHeader.Dropdown id="app-user-dropdown" title={title}>
-        <MenuItem onClick={this.onLogoutUser}>Log Out</MenuItem>
-      </PageHeader.Dropdown>
-    );
-  }
-
   render() {
-    const { isDropdownOpen } = this.state;
-    const userDropdownItems = [<DropdownItem onClick={this.onLogoutUser}>Log out</DropdownItem>];
+    const { isUserDropdownOpen } = this.state;
 
-    const PageToolbar = (
+    const logoProps = {
+      onClick: () => this.onTitleClick()
+    };
+
+    const MastheadToolbar = (
       <Toolbar>
         <ToolbarGroup className={css(accessibleStyles.screenReader, accessibleStyles.visibleOnLg)}>
           <ToolbarItem className={css(accessibleStyles.screenReader, accessibleStyles.visibleOnMd)}>
             <Dropdown
               isPlain
               position="right"
-              onSelect={this.onDropdownSelect}
-              isOpen={isDropdownOpen}
+              onSelect={this.onUserDropdownSelect}
+              isOpen={isUserDropdownOpen}
               toggle={
-                <DropdownToggle onToggle={this.onDropdownToggle}>
+                <DropdownToggle onToggle={this.onUserDropdownToggle}>
                   {window.localStorage.getItem('currentUserName')}
                 </DropdownToggle>
               }
-              dropdownItems={userDropdownItems}
-            />
+            >
+              <DropdownItem key="logout" component="button" href="#logout" onClick={this.onLogoutUser}>
+                Log out
+              </DropdownItem>
+            </Dropdown>
           </ToolbarItem>
         </ToolbarGroup>
       </Toolbar>
     );
 
-    const logoProps = {
-      onClick: () => this.onTitleClick(),
-      target: '_blank'
-    };
-
     return (
       <PageHeader
         logo={<Brand src={brandImg} alt="Red Hat Solution Explorer" />}
         logoProps={logoProps}
-        toolbar={PageToolbar}
+        toolbar={MastheadToolbar}
       />
     );
   }
@@ -171,6 +120,12 @@ Masthead.defaultProps = {
     push: noop
   }
 };
+
+// function mapStateToProps(state) {
+//   return {
+//     history: this.props.history
+//   };
+// }
 
 const mapDispatchToProps = dispatch => ({
   logoutUser: () => dispatch(reduxActions.userActions.logoutUser())
