f0485f280d64d471fe04128f297e623ee6a494ca
Mark Franceschelli <mfrances@redhat.com>
2019-01-31 14:29:48 -0500

    fix compile and test errors

--- src/pages/landing/landingPage.js
+++ src/pages/landing/landingPage.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
 import { noop } from 'patternfly-react';
-import { BackgroundImage, BackgroundImageSrc, Grid, GridItem, Page, PageSection } from '@patternfly/react-core';
+import { Grid, GridItem, Page, PageSection } from '@patternfly/react-core';
 import PfMasthead from '../../components/masthead/masthead';
 import TutorialDashboard from '../../components/tutorialDashboard/tutorialDashboard';
 import InstalledAppsView from '../../components/installedAppsView/InstalledAppsView';
@@ -17,17 +17,8 @@ class LandingPage extends React.Component {
   render() {
     const { walkthroughServices, middlewareServices, user } = this.props;
 
-    const bgImages = {
-      [BackgroundImageSrc.xs]: '/assets/images/pfbg_576.jpg',
-      [BackgroundImageSrc.xs2x]: '/assets/images/pfbg_576@2x.jpg',
-      [BackgroundImageSrc.sm]: '/assets/images/pfbg_768.jpg',
-      [BackgroundImageSrc.sm2x]: '/assets/images/pfbg_768@2x.jpg',
-      [BackgroundImageSrc.lg]: '/assets/images/pfbg_1200.jpg',
-      [BackgroundImageSrc.filter]: '/assets/images/background-filter.svg#image_overlay'
-    };
     return (
       <React.Fragment>
-        <BackgroundImage src={bgImages} />
         <Page>
           <PfMasthead />
           <PageSection className="integr8ly-landing-page-tutorial-dashboard-section pf-u-p-0">
