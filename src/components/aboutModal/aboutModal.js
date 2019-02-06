4869186fade3e9c01ea2616ddc94de1fd4347cdd
mfrances17 <39063664+mfrances17@users.noreply.github.com>
2019-02-06 15:03:24 -0500

    INTLY-468: Add About Modal with Web App release info (#395)
    * add about modal
    
    * add error boundaries and fix tests
    
    * update snap
    
    * add alt txt in modal and tests

--- src/components/aboutModal/aboutModal.js
+++ src/components/aboutModal/aboutModal.js
@@ -1,126 +1,55 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { AboutModal as PfAboutModal, TextContent, TextList, TextListItem } from '@patternfly/react-core';
 import { detect } from 'detect-browser';
-import { AboutModal as PfAboutModal } from 'patternfly-react';
-import { connect, reduxTypes, store } from '../../redux';
-import { copyClipboard, UI_COMMIT_HASH } from '../../common/helpers';
-import logo from '../../img/logo-alt.svg';
-import titleImg from '../../img/brand-alt.svg';
+import solutionsExplorerLogo from '../../img/logo-alt.svg';
+import redHatLogo from '../../img/Logo_RH_RGB_Reverse.png';
+
+const pkgJson = require('../../../package.json');
 
 class AboutModal extends React.Component {
-  selectElement = React.createRef();
-
   state = {
-    copied: false,
-    timer: null
+    isModalOpen: true
   };
 
-  onClick = () => {
-    const { timer } = this.state;
-    const selectElement = this.selectElement.current;
-    const success = copyClipboard(selectElement.innerText);
-
-    clearTimeout(timer);
-
-    this.setState(
-      {
-        copied: success
-      },
-      () => this.resetStateTimer()
-    );
+  handleModalToggle = () => {
+    this.setState(({ isModalOpen }) => ({
+      isModalOpen: !isModalOpen
+    }));
   };
-
-  onClose = () => {
-    const { onClose } = this.props;
-
-    if (onClose) {
-      onClose();
-    } else {
-      store.dispatch({
-        type: reduxTypes.aboutModalTypes.ABOUT_MODAL_HIDE
-      });
-    }
-  };
-
-  onKeyUp = () => {
-    this.onClick();
-  };
-
-  resetStateTimer() {
-    const { resetTimer } = this.props;
-
-    const timer = setTimeout(
-      () =>
-        this.setState({
-          copied: false
-        }),
-      resetTimer
-    );
-
-    this.setState({ timer });
-  }
 
   render() {
-    const { copied } = this.state;
-    const { commitHash, show, session } = this.props;
+    const { isModalOpen } = this.state;
     const browser = detect();
 
-    const props = {
-      show,
-      onHide: this.onClose,
-      logo,
-      productTitle: <img src={titleImg} alt="PatternFly Enterprise Application" />,
-      trademarkText: `Copyright (c) ${new Date().getFullYear()} Red Hat Inc.`,
-      altLogo: 'PatternFly Enterprise Application'
-    };
-
     return (
-      <PfAboutModal {...props}>
-        <div
-          role="button"
-          ref={this.selectElement}
-          onClick={this.onClick}
-          onKeyUp={this.onKeyUp}
-          tabIndex={0}
-          aria-label="About modal, copy application information"
-          aria-live="polite"
+      <React.Fragment>
+        <PfAboutModal
+          isOpen={isModalOpen}
+          onClose={this.handleModalToggle}
+          productName="Red Hat Solution Explorer"
+          trademark={`Copyright (c) ${new Date().getFullYear()} Red Hat, Inc.`}
+          brandImageSrc={redHatLogo}
+          brandImageAlt="Red Hat logo"
+          logoImageSrc={solutionsExplorerLogo}
+          logoImageAlt="Red Hat Solutions Explorer logo"
         >
-          <PfAboutModal.Versions>
-            {commitHash && <PfAboutModal.VersionItem label="Version Hash" versionText={commitHash || ''} />}
-            {session && <PfAboutModal.VersionItem label="Username" versionText={session.username || ''} />}
-            {browser && (
-              <PfAboutModal.VersionItem label="Browser Version" versionText={`${browser.name} ${browser.version}`} />
-            )}
-            {browser && <PfAboutModal.VersionItem label="Browser OS" versionText={browser.os || ''} />}
-          </PfAboutModal.Versions>
-        </div>
-        {copied && <div className="integr8ly-app-about-modal-copy">Copied</div>}
-      </PfAboutModal>
+          <TextContent>
+            <TextList component="dl">
+              <TextListItem component="dt">Webapp version</TextListItem>
+              <TextListItem component="dd">{pkgJson.version}</TextListItem>
+              <TextListItem component="dt">User Name</TextListItem>
+              <TextListItem component="dd">{window.localStorage.getItem('currentUserName')}</TextListItem>
+              <TextListItem component="dt">Browser Version</TextListItem>
+              <TextListItem component="dd">
+                {browser ? browser.name : ' '} {browser ? browser.version : ' '}
+              </TextListItem>
+              <TextListItem component="dt">Browser OS</TextListItem>
+              <TextListItem component="dd">{browser ? browser.os : ' '}</TextListItem>
+            </TextList>
+          </TextContent>
+        </PfAboutModal>
+      </React.Fragment>
     );
   }
 }
-
-AboutModal.propTypes = {
-  commitHash: PropTypes.string,
-  onClose: PropTypes.func,
-  resetTimer: PropTypes.number,
-  session: PropTypes.shape({
-    username: PropTypes.string
-  }),
-  show: PropTypes.bool.isRequired
-};
-
-AboutModal.defaultProps = {
-  commitHash: UI_COMMIT_HASH,
-  resetTimer: 3000,
-  session: {
-    username: null
-  },
-  onClose: null
-};
-
-const mapStateToProps = state => ({ ...state.aboutModalReducers, session: state.userReducers.session });
-
-const ConnectedAboutModal = connect(mapStateToProps)(AboutModal);
-
-export { ConnectedAboutModal as default, ConnectedAboutModal, AboutModal };
+export { AboutModal as default, AboutModal };
