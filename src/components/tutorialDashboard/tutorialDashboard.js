67066740a5fd31ba7300e40b59504d6a548cac55
Evan Shortiss <evanshortiss@gmail.com>
2019-02-11 14:45:56 -0800

    feat: add basic walkthrough sorting

--- src/components/tutorialDashboard/tutorialDashboard.js
+++ src/components/tutorialDashboard/tutorialDashboard.js
@@ -7,8 +7,15 @@ import TutorialCard from '../tutorialCard/tutorialCard';
 
 const TutorialDashboard = props => {
   const { walkthroughs, userProgress } = props;
-  const cards = [];
-  walkthroughs.map((walkthrough, i) => {
+
+  function walkthroughSorter(w1, w2) {
+    const a = `${w1.id} ${w1.title}`;
+    const b = `${w2.id} ${w2.title}`;
+
+    return a < b ? -1 : 1;
+  }
+
+  const cards = walkthroughs.sort(walkthroughSorter).map((walkthrough, i) => {
     const currentProgress = userProgress[walkthrough.id];
     let startedText;
     if (currentProgress === undefined) startedText = 'Get Started';
