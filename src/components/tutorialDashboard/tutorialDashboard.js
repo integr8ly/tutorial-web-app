88bd78e3d8d74681465d18addff552d21b50107f
mfrances <mfrances@redhat.com>
2019-04-11 11:47:02 -0400

    separate wts by repo heading

--- src/components/tutorialDashboard/tutorialDashboard.js
+++ src/components/tutorialDashboard/tutorialDashboard.js
@@ -8,6 +8,28 @@ import TutorialCard from '../tutorialCard/tutorialCard';
 const TutorialDashboard = props => {
   const { walkthroughs, userProgress } = props;
 
+  function filterWalkthroughs(walkthrus, filter) {
+    let result = [];
+    if (walkthrus[0]) {
+      result = walkthrus.filter(walkthru => walkthru.walkthroughLocationInfo.remote === filter);
+      return result;
+    }
+    return result;
+  }
+
+  function getRepos(walkthroughs) {
+    const repos = [];
+
+    for (let i = 0; i < walkthroughs.length; i++) {
+      const currentWalkthrough = walkthroughs[i];
+      const repo = currentWalkthrough.walkthroughLocationInfo.remote;
+      if (!repos.includes(repo)) {
+        repos.push(repo);
+      }
+    }
+    return repos;
+  }
+
   function walkthroughSorter(w1, w2) {
     const a = `${w1.id} ${w1.title}`;
     const b = `${w2.id} ${w2.title}`;
@@ -15,54 +37,75 @@ const TutorialDashboard = props => {
     return a < b ? -1 : 1;
   }
 
-  const cards = walkthroughs.sort(walkthroughSorter).map((walkthrough, i) => {
-    const currentProgress = userProgress[walkthrough.id];
-    let startedText;
-    if (currentProgress === undefined) startedText = 'Get Started';
-    else if (currentProgress.progress === 100) startedText = 'Completed';
-    else startedText = 'Resume';
+  function addCategory(walkthroughs) {
+    const repo = walkthroughs[0].walkthroughLocationInfo.remote;
+    const repoParts = repo.split('/');
+    const repoLastPart = repoParts.length - 1;
+    const repoCategory = repoParts[repoLastPart];
+    const htmlCategory = <h3 className="pf-c-title pf-m-2xl pf-u-mt-sm">{repoCategory} Solution Patterns</h3>;
+    return htmlCategory;
+  }
 
-    return (
-      <GalleryItem key={walkthrough.id}>
-        <TutorialCard
-          title={walkthrough.title}
-          getStartedLink={
-            currentProgress !== undefined && currentProgress.task + 1 === currentProgress.totalTasks
-              ? `/tutorial/${walkthrough.id}`
-              : `/tutorial/${walkthrough.id}/${currentProgress === undefined ? '' : `task/${currentProgress.task}`}`
-          }
-          getStartedText={startedText}
-          getStartedIcon={
-            <Icon
-              type="fa"
-              name={
-                currentProgress !== undefined && currentProgress.progress === 100
-                  ? 'check-circle pf-u-mr-xs integr8ly-c-card__status--complete-icon'
-                  : 'arrow-circle-right pf-u-mr-sm'
+  function addAll(walkthroughs) {
+    const allRepos = getRepos(walkthroughs);
+    const htmlSnippet = [];
+
+    for (let i = 0; i < allRepos.length; i++) {
+      const filteredWalkthroughs = filterWalkthroughs(walkthroughs, allRepos[i]);
+
+      const cards = filteredWalkthroughs.sort(walkthroughSorter).map((walkthrough, i) => {
+        const currentProgress = userProgress[walkthrough.id];
+        let startedText;
+        if (currentProgress === undefined) startedText = 'Get Started';
+        else if (currentProgress.progress === 100) startedText = 'Completed';
+        else startedText = 'Resume';
+
+        return (
+          <GalleryItem key={walkthrough.id}>
+            <TutorialCard
+              title={walkthrough.title}
+              getStartedLink={
+                currentProgress !== undefined && currentProgress.task + 1 === currentProgress.totalTasks
+                  ? `/tutorial/${walkthrough.id}`
+                  : `/tutorial/${walkthrough.id}/${currentProgress === undefined ? '' : `task/${currentProgress.task}`}`
               }
-            />
-          }
-          minsIcon={<ClockIcon className="pf-u-mr-sm" />}
-          progress={currentProgress === undefined ? 0 : currentProgress.progress}
-          mins={walkthrough.time}
-        >
-          <p>{walkthrough.shortDescription}</p>
-        </TutorialCard>
-      </GalleryItem>
-    );
-  });
-
-  return (
-    <div className="integr8ly-tutorial-dashboard pf-u-mb-0">
-      <div className="integr8ly-tutorial-dashboard-title pf-u-display-flex pf-u-py-sm">
-        <h1 className="pf-c-title pf-m-4xl pf-u-mt-sm">Start with a walkthrough</h1>
-        <div className="integr8ly-walkthrough-counter pf-u-mt-lg pf-u-mr-md pf-u-text-align-right pf-m-sm">
-          <strong>{walkthroughs.length} walkthroughs</strong>
+              getStartedText={startedText}
+              getStartedIcon={
+                <Icon
+                  type="fa"
+                  name={
+                    currentProgress !== undefined && currentProgress.progress === 100
+                      ? 'check-circle pf-u-mr-xs integr8ly-c-card__status--complete-icon'
+                      : 'arrow-circle-right pf-u-mr-sm'
+                  }
+                />
+              }
+              minsIcon={<ClockIcon className="pf-u-mr-sm" />}
+              progress={currentProgress === undefined ? 0 : currentProgress.progress}
+              mins={walkthrough.time}
+            >
+              <p>{walkthrough.shortDescription}</p>
+            </TutorialCard>
+          </GalleryItem>
+        );
+      });
+      htmlSnippet.push(
+        <div className="integr8ly-tutorial-dashboard-title pf-u-display-flex pf-u-py-sm">
+          {addCategory(filteredWalkthroughs)}
+          <div className="integr8ly-walkthrough-counter pf-u-mt-lg pf-u-mr-md pf-u-text-align-right pf-m-sm">
+            {filteredWalkthroughs.length === 1 ? (
+              <strong>{filteredWalkthroughs.length} walkthough</strong>
+            ) : (
+              <strong>{filteredWalkthroughs.length} walkthoughs</strong>
+            )}
+          </div>
         </div>
-      </div>
-      <Gallery gutter="md">{cards}</Gallery>
-    </div>
-  );
+      );
+      htmlSnippet.push(<Gallery gutter="md">{cards}</Gallery>);
+    }
+    return htmlSnippet;
+  }
+  return <div className="integr8ly-tutorial-dashboard pf-u-mb-0">{addAll(walkthroughs)}</div>;
 };
 
 TutorialDashboard.propTypes = {
