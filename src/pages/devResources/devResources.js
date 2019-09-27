22f5b97dc71b50cb75e26d99d162716a8a50ac71
mfrances <mfrances@redhat.com>
2019-09-27 13:45:22 -0400

    working version

--- src/pages/devResources/devResources.js
+++ src/pages/devResources/devResources.js
@@ -21,161 +21,56 @@ import { noop } from 'patternfly-react';
 import RoutedConnectedMasthead from '../../components/masthead/masthead';
 import { connect, reduxActions } from '../../redux';
 import Breadcrumb from '../../components/breadcrumb/breadcrumb';
-// import { setUserWalkthroughs, getUserWalkthroughs } from '../../services/walkthroughServices';
 
 class DevResourcesPage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      clusterId: '',
-      loggingUrl: '',
-      clusterType: ''
-    };
-  }
-
-  componentDidMount() {
-    this.getClusterType();
-  }
-
-  componentDidUpdate(prevState) {
+  getClusterUrls = () => {
     const uri = window.location.href;
-    if (this.state.clusterType !== prevState.clusterType) {
-      this.getClusterId(uri);
-    }
-  }
-
-  // if mockdata, use localhost as the cluster type, otherwise get the type via window.OPENSHIFT_CONFIG
-  // which is itself derived from an env var in server.js
-  getClusterType = () => {
-    let cType = '';
-
-    if (window.OPENSHIFT_CONFIG) {
-      if (window.OPENSHIFT_CONFIG.mockData) {
-        cType = 'localhost';
-        console.log(`Running locally, the clusterType is: ${cType}`);
-      } else {
-        cType = window.OPENSHIFT_CONFIG.clusterType;
-        console.log(`Running on server, the clusterType is: ${cType}`);
-      }
-    }
-    // this.setState({ clusterType: cType }, () => console.log(this.state.clusterType));
-
-    this.setState({ clusterType: cType }, () => console.log(this.state.clusterType));
-
-    // return clusterType;
-  };
-
-  // method that retrieves the clusterId from a URL
-  getClusterId = url => {
+    let clusterType = '';
     let urlParts = [];
-    // let clusterId = '';
-    // let loggingUrl = '';
-    // const clusterType = this.getClusterType();
-    urlParts = new URL(url).host.split('.');
+    const urls = { loggingUrl: '', apiUrl: '', registryUrl: '' };
+
+    urlParts = new URL(uri).host.split('.');
     const [pocClusterId, , rhpdsClusterId] = urlParts;
 
-    switch (this.state.clusterType) {
-      case 'rhpds':
-        // 'https://tutorial-web-app-webapp.apps.puma.openshiftworkshop.com/'
-        // 'https://tutorial-web-app-webapp.apps.puma.open.redhat.com/'
-
-        this.setState = ({
-          clusterId: rhpdsClusterId,
-          loggingUrl: `https://kibana.apps.${rhpdsClusterId}.openshiftworkshop.com`
-        },
-        () => {
-          console.log('loggingUrl =', this.state.loggingUrl);
-        });
-        console.log(this.state);
-        break;
-      // clusterId = rhpdsClusterId;
-      //     loggingUrl = `https://kibana.apps.${clusterId}.openshiftworkshop.com`;
-      //     break;
-      case 'poc':
-        this.setState = ({
-          clusterId: pocClusterId,
-          loggingUrl: `https://kibana.apps.${pocClusterId}.openshiftworkshop.com`
-        },
-        () => {
-          console.log('loggingUrl =', this.state.loggingUrl);
-        });
-        console.log(this.state);
-        break;
-      // 'https://puma.rhmi.io/'
-      // clusterId = pocClusterId;
-      // break;
-      case 'osd':
-        this.setState = ({
-          clusterId: pocClusterId,
-          loggingUrl: `https://kibana.apps.${pocClusterId}.openshiftworkshop.com`
-        },
-        () => {
-          console.log('loggingUrl =', this.state.loggingUrl);
-        });
-        console.log(this.state);
-        break;
-      // `https://puma.openshift.com`
-      // clusterId = pocClusterId;
-      // break;
-      default:
-        this.setState = ({
-          clusterId: pocClusterId,
-          loggingUrl: `https://kibana.apps.${pocClusterId}.openshiftworkshop.com`
-        },
-        () => {
-          console.log('loggingUrl =', this.state.loggingUrl);
-        });
-        console.log(this.state);
-      //   clusterId = 'localhost';
-      // loggingUrl = `https://kibana.apps.${clusterId}.openshiftworkshop.com`;
+    if (window.OPENSHIFT_CONFIG) {
+      clusterType = window.OPENSHIFT_CONFIG.mockData ? 'localhost' : window.OPENSHIFT_CONFIG.clusterType;
     }
 
-    // question: is clusterId just the hostname? if so, can try using window.location.hostname
-    // see this: https://guide.freecodecamp.org/javascript/window-location/
-
-    // uriString = uriString.replace('https://master.', '');
-    // uriString = uriString.substring(0, uriString.indexOf('.'));
-
-    console.log(`Original Url: ${url}`);
-    console.log(`Cluster type: ${this.state.clusterType}`);
-    console.log(`Cluster ID: ${this.state.clusterId}`);
-    console.log(`Results of window.location.hostname: ${window.location.hostname}`);
-    console.log(`Results of window.location.host: ${window.location.host}`);
-
-    // return clusterId;
+    switch (clusterType) {
+      case 'rhpds':
+        urls.loggingUrl = `https://kibana.apps.${rhpdsClusterId}.open.redhat.com`;
+        urls.apiUrl = `https://master.${rhpdsClusterId}.open.redhat.com/api`;
+        urls.registryUrl = `https://registry-console-default.apps.${rhpdsClusterId}.open.redhat.com`;
+        break;
+      case 'poc':
+        urls.loggingUrl = `https://kibana.apps.${pocClusterId}.rhmi.io`;
+        urls.apiUrl = `https://master.${pocClusterId}.rhmi.io/api`;
+        urls.registryUrl = `https://registry-console-default.apps.${pocClusterId}.rhmi.io`;
+        break;
+      case 'osd':
+        urls.loggingUrl = `https://logs.${pocClusterId}.openshift.com`;
+        urls.apiUrl = `https://api.${pocClusterId}.openshift.com`;
+        urls.registryUrl = `https://registry.${pocClusterId}.openshift.com`;
+        break;
+      case 'localhost':
+        urls.loggingUrl = `No logging URL when running locally`;
+        urls.apiUrl = `No API URL when running locally`;
+        urls.registryUrl = `No registry URL when running locally`;
+        break;
+      default:
+        urls.loggingUrl = `Unknown logging URL`;
+        urls.apiUrl = `Unknown API URL`;
+        urls.registryUrl = `Unknown registry URL`;
+    }
+    return urls;
   };
 
   render() {
-    // this.parseClusterId();
-    // const {} = this.state;
     const loggingTooltip = 'The URL for the aggregate logging Kibana interface.';
     const apiTooltip = 'The URL for the OpenShift and Kubernetes REST API.';
     const registryTooltip = 'The URL for the private image registry.';
 
-    const loggingUrl = this.state.loggingUrl;
-
-    // const rhpdsUri = 'https://tutorial-web-app-webapp.apps.uxddev-b2b9.openshiftworkshop.com/';
-    // const rhmiUri = 'https://puma.rhmi.io/';
-    // const clusterType = 'rhpds';
-    const uri = window.location.href;
-
-    // const clusterId = 'uxddev-17f0'; // MF080519 - get this from the existing variable
-    // const clusterId = this.getClusterId(uri); // MF080519 - get this from the existing variable
-    const clusterId = this.state.clusterId;
-
-    // MF080519 - need to get this from env var and populate with the correct one
-    const osdLoggingUrl = `https://logs.${clusterId}.openshift.com`;
-    // const pocLoggingUrl = `https://kibana.apps.${clusterId}.rhmi.io`;
-    // const pdsLoggingUrl = `https://kibana.apps.${clusterId}.openshiftworkshop.com`;
-
-    const osdApiUrl = `https://api.${clusterId}.openshift.com`;
-    // const pocApiUrl = `https://master.${clusterId}.rhmi.io/api`;
-    // const pdsApiUrl = `https://master.${clusterId}.openshiftworkshop.com/api`;
-
-    const osdRegistryUrl = `https://registry.${clusterId}.openshift.com`;
-    // const pocRegistryUrl = `https://registry-console-default.apps.${clusterId}.rhmi.io`;
-    // const pdsRegistryUrl = `https://registry-console-default.apps.${clusterId}.openshiftworkshop.com`;
+    const { loggingUrl, apiUrl, registryUrl } = this.getClusterUrls();
 
     return (
       <Page className="pf-u-h-100vh">
@@ -201,8 +96,12 @@ class DevResourcesPage extends React.Component {
                       </span>
                     </Tooltip>
                   </h4>
-                  <Button variant="link" icon={<ExternalLinkSquareAltIcon />}>
-                    {/* {osdLoggingUrl} */}
+                  <Button
+                    variant="link"
+                    icon={<ExternalLinkSquareAltIcon />}
+                    component="a"
+                    href={loggingUrl === `No logging URL when running locally` ? '#' : loggingUrl}
+                  >
                     {loggingUrl}
                   </Button>{' '}
                 </CardBody>
@@ -216,7 +115,7 @@ class DevResourcesPage extends React.Component {
                     </Tooltip>
                   </h4>
                   <ClipboardCopy id="api-cc" isReadOnly>
-                    {osdApiUrl}
+                    {apiUrl}
                   </ClipboardCopy>
                 </CardBody>
                 <CardBody>
@@ -229,7 +128,7 @@ class DevResourcesPage extends React.Component {
                     </Tooltip>
                   </h4>
                   <ClipboardCopy id="registry-cc" isReadOnly>
-                    {osdRegistryUrl}
+                    {registryUrl}
                   </ClipboardCopy>
                 </CardBody>
                 <CardFooter />
