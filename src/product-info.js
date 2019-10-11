aa79fb54d3b692603ffe53a7b3b2a8a9904a28e6
Tiffany Nolan <tnolan@redhat.com>
2019-10-11 14:00:48 -0400

    Add placeholder properties
    ##Motivation
    https://issues.jboss.org/browse/INTLY-3714
    
    ##What
    Add 2 new properties to each product. One for the primary task and one for a description. 
    
    ##Why
    This information will be used and displayed in a data list on the new Solution Explorer home page.

--- src/product-info.js
+++ src/product-info.js
@@ -2,45 +2,65 @@
 export default {
   'amq-online-standard': {
     prettyName: 'Red Hat AMQ Online',
-    gaStatus: 'GA'
+    gaStatus: 'GA',
+    primaryTask: '',
+    description:''
   },
   fuse: {
     prettyName: 'Red Hat Fuse Online',
     gaStatus: 'GA',
+    primaryTask: '',
+    description:'',
     // Don't show the fuse instance only intended for walkthroughs
     // in the installed apps list
     hidden: true
   },
   'fuse-managed': {
     prettyName: 'Red Hat Fuse Online (Shared)',
-    gaStatus: 'GA'
+    gaStatus: 'GA',
+    primaryTask: '',
+    description:''
   },
   codeready: {
     prettyName: 'Red Hat CodeReady Workspaces',
-    gaStatus: 'GA'
+    gaStatus: 'GA',
+    primaryTask: '',
+    description:''
   },
   launcher: {
     prettyName: 'Red Hat Developer Launcher',
-    gaStatus: 'GA'
+    gaStatus: 'GA',
+    primaryTask: '',
+    description:''
   },
   '3scale': {
     prettyName: 'Red Hat 3scale API Management Platform',
-    gaStatus: 'GA'
+    gaStatus: 'GA',
+    primaryTask: '',
+    description:''
   },
   apicurio: {
     prettyName: 'API Designer',
-    gaStatus: 'GA'
+    gaStatus: 'GA',
+    primaryTask: '',
+    description:''
   },
   mdc: {
     prettyName: 'Mobile Developer Console',
-    gaStatus: 'preview'
+    gaStatus: 'preview',
+    primaryTask: '',
+    description:''
   },
   rhsso: {
     prettyName: 'Red Hat Single Sign-On (Cluster)',
-    gaStatus: 'GA'
+    gaStatus: 'GA',
+    primaryTask: '',
+    description:''
   },
   'user-rhsso': {
     prettyName: 'Red Hat Single Sign-On',
-    gaStatus: 'GA'
+    gaStatus: 'GA',
+    primaryTask: '',
+    description:''
   }
 };
