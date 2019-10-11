164b1326d1bd0963f9ef59fdd2877e3f6e99bdd2
Tiffany Nolan <tnolan@redhat.com>
2019-10-11 16:18:40 -0400

    INTLY-3714: Add primary task and description info to product-info.js (#518)
    * Add placeholder properties
    
    ##Motivation
    https://issues.jboss.org/browse/INTLY-3714
    
    ##What
    Add 2 new properties to each product. One for the primary task and one for a description. 
    
    ##Why
    This information will be used and displayed in a data list on the new Solution Explorer home page.
    
    * Update product-info.js
    
    Added info from the Copy Doc.
    
    * Fixed spacing after the new description prop.
    
    * Update product-info.js

--- src/product-info.js
+++ src/product-info.js
@@ -2,45 +2,68 @@
 export default {
   'amq-online-standard': {
     prettyName: 'Red Hat AMQ Online',
-    gaStatus: 'GA'
+    gaStatus: 'GA',
+    primaryTask: 'Provision messaging',
+    description: 'Fast, lightweight, and secure messaging for Internet-scale applications.'
   },
   fuse: {
     prettyName: 'Red Hat Fuse Online',
     gaStatus: 'GA',
+    primaryTask: 'Create integrations',
+    description:
+      'A facility to obtain data from an application or service, operate on that data, and then send the data to another application or service without writing code.',
     // Don't show the fuse instance only intended for walkthroughs
     // in the installed apps list
     hidden: true
   },
   'fuse-managed': {
     prettyName: 'Red Hat Fuse Online (Shared)',
-    gaStatus: 'GA'
+    gaStatus: 'GA',
+    primaryTask: 'Create integrations',
+    description:
+      'A facility to obtain data from an application or service, operate on that data, and then send the data to another application or service without writing code.'
   },
   codeready: {
     prettyName: 'Red Hat CodeReady Workspaces',
-    gaStatus: 'GA'
+    gaStatus: 'GA',
+    primaryTask: 'Online IDE',
+    description: 'A developer workspace server and cloud IDE.'
   },
   launcher: {
     prettyName: 'Red Hat Developer Launcher',
-    gaStatus: 'GA'
+    gaStatus: 'GA',
+    primaryTask: 'Deliver apps',
+    description: 'A platform for continuous application delivery, with a set of templates.'
   },
   '3scale': {
     prettyName: 'Red Hat 3scale API Management Platform',
-    gaStatus: 'GA'
+    gaStatus: 'GA',
+    primaryTask: 'Manage APIs',
+    description: 'A platform that provides RESTful API Management.'
   },
   apicurio: {
     prettyName: 'API Designer',
-    gaStatus: 'GA'
+    gaStatus: 'GA',
+    primaryTask: 'Design APIs',
+    description: 'The Apicurito RESTful API visual designer.'
   },
   mdc: {
     prettyName: 'Mobile Developer Console',
-    gaStatus: 'preview'
+    gaStatus: 'preview',
+    primaryTask: 'Create mobile apps',
+    description: 'Allows you register your mobile app and associate it with Mobile Developer Services.'
   },
   rhsso: {
-    prettyName: 'Red Hat Single Sign-On (Cluster)',
-    gaStatus: 'GA'
+    prettyName: 'Red Hat Single Sign-On',
+    gaStatus: 'GA',
+    primaryTask: 'Manage users (Admins only)',
+    description: 'A single sign-on solution for web apps, mobile apps, and RESTful web services.'
   },
   'user-rhsso': {
-    prettyName: 'Red Hat Single Sign-On',
-    gaStatus: 'GA'
+    prettyName: 'End-user Red Hat Single Sign-On',
+    gaStatus: 'GA',
+    primaryTask: 'Setup authentication',
+    description:
+      'Admins can use our single sign-on solution for managing cluster users. Contact your local customer admin if you do not have access to this console. '
   }
 };
