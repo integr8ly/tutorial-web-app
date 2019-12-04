6696a746ba5392eb452e243b88fc28ac72158f0f
mfrances17 <39063664+mfrances17@users.noreply.github.com>
2019-12-04 09:56:58 -0500

    INTLY-3692: Dashboard changes (#529)
    * various dash tab updates
    
    * all solution patterns tab
    
    * fix stylelint issues
    
    * clean up initial tab
    
    * clean up styles
    
    * latest updates
    
    * launch buttons and fix styles
    
    * css style fixes
    
    * style and product-info updates
    
    * fix probably lint issue
    
    * remove self managed section and ver placeholders
    
    * fix lint issue
    
    * more lint issues
    
    * fix broken tests
    
    * bump version

--- src/product-info.js
+++ src/product-info.js
@@ -1,10 +1,22 @@
 // This file needs to live inside src to be importable
 export default {
-  'amq-online-standard': {
-    prettyName: 'Red Hat AMQ Online',
+  codeready: {
+    prettyName: 'Red Hat CodeReady Workspaces',
     gaStatus: 'GA',
-    primaryTask: 'Provision messaging',
-    description: 'Fast, lightweight, and secure messaging for Internet-scale applications.'
+    primaryTask: 'Online IDE',
+    description: 'A developer workspace server and cloud IDE.'
+  },
+  apicurio: {
+    prettyName: 'API Designer',
+    gaStatus: 'GA',
+    primaryTask: 'Design APIs',
+    description: 'The Apicurito RESTful API visual designer.'
+  },
+  '3scale': {
+    prettyName: 'Red Hat 3scale API Management Platform',
+    gaStatus: 'GA',
+    primaryTask: 'Manage APIs',
+    description: 'A platform that provides RESTful API Management.'
   },
   fuse: {
     prettyName: 'Red Hat Fuse Online',
@@ -23,29 +35,11 @@ export default {
     description:
       'A facility to obtain data from an application or service, operate on that data, and then send the data to another application or service without writing code.'
   },
-  codeready: {
-    prettyName: 'Red Hat CodeReady Workspaces',
+  'amq-online-standard': {
+    prettyName: 'Red Hat AMQ Online',
     gaStatus: 'GA',
-    primaryTask: 'Online IDE',
-    description: 'A developer workspace server and cloud IDE.'
-  },
-  launcher: {
-    prettyName: 'Red Hat Developer Launcher',
-    gaStatus: 'GA',
-    primaryTask: 'Deliver apps',
-    description: 'A platform for continuous application delivery, with a set of templates.'
-  },
-  '3scale': {
-    prettyName: 'Red Hat 3scale API Management Platform',
-    gaStatus: 'GA',
-    primaryTask: 'Manage APIs',
-    description: 'A platform that provides RESTful API Management.'
-  },
-  apicurio: {
-    prettyName: 'API Designer',
-    gaStatus: 'GA',
-    primaryTask: 'Design APIs',
-    description: 'The Apicurito RESTful API visual designer.'
+    primaryTask: 'Provision messaging',
+    description: 'Fast, lightweight, and secure messaging for Internet-scale applications.'
   },
   unifiedpush: {
     prettyName: 'Push Notification Service',
@@ -53,17 +47,23 @@ export default {
     primaryTask: 'Mobile Push Messaging',
     description: 'Provides messaging services for Android and iOS'
   },
-  rhsso: {
-    prettyName: 'Red Hat Single Sign-On',
-    gaStatus: 'GA',
-    primaryTask: 'Manage users (Admins only)',
-    description: 'A single sign-on solution for web apps, mobile apps, and RESTful web services.'
-  },
   'user-rhsso': {
     prettyName: 'End-user Red Hat Single Sign-On',
     gaStatus: 'GA',
     primaryTask: 'Setup authentication',
     description:
       'Admins can use our single sign-on solution for managing cluster users. Contact your local customer admin if you do not have access to this console. '
+  },
+  rhsso: {
+    prettyName: 'Red Hat Single Sign-On',
+    gaStatus: 'GA',
+    primaryTask: 'Manage users (Admins only)',
+    description: 'A single sign-on solution for web apps, mobile apps, and RESTful web services.'
+  },
+  launcher: {
+    prettyName: 'Red Hat Developer Launcher',
+    gaStatus: 'GA',
+    primaryTask: 'Deliver apps',
+    description: 'A platform for continuous application delivery, with a set of templates.'
   }
 };
