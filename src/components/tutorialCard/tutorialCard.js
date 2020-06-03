97df76208e9a33e1c0bf6e016a2e75eb097ceeb7
redallen <zallen@redhat.com>
2020-06-03 14:24:39 -0400

    run pf-codemods --fix

--- src/components/tutorialCard/tutorialCard.js
+++ src/components/tutorialCard/tutorialCard.js
@@ -6,7 +6,7 @@ import {
   CardBody,
   CardFooter,
   Progress,
-  ProgressMeasureLocation
+  ProgressMeasureLocation, CardTitle, CardHeaderMain
 } from '@patternfly/react-core';
 import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
@@ -25,9 +25,9 @@ const TutorialCard = props => (
       props.history.push(props.getStartedLink);
     }}
   >
-    <CardHeader>
+    <CardTitle>
       <h3 className="pf-c-title pf-m-xl">{props.title}</h3>
-    </CardHeader>
+    </CardTitle>
     <CardBody>{props.children}</CardBody>
     <CardFooter>
       <div className="integr8ly-c-card__info pf-u-w-100">
